0227ffb36eb2b3e457711f9656863a6d45669e32
a.salman <aliayadalsalman@gmail.com>
2023-12-29 12:53:36 +0100

    Cookiecutter Template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Heart Rate Variability Processing with Artificial Intelligence Techniques',
+    author='Ernesto Iadanza Lab.',
+    license='MIT',
+)
